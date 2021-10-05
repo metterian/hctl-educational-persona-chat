d56d74ef3faf6578e864939884165cdff4ee2ca0
metterian <dzzy6505@gmail.com>
2021-10-05 06:26:36 +0000

    fix: load top_filtering as package

--- ChatBot.py
+++ ChatBot.py
@@ -4,17 +4,27 @@ from itertools import chain
 import warnings
 import torch
 import torch.nn.functional as F
+from train import add_special_tokens_
 
+from interact import top_filtering
 
 from transformers import OpenAIGPTLMHeadModel, OpenAIGPTTokenizer, GPT2LMHeadModel, GPT2Tokenizer
 from train import SPECIAL_TOKENS, build_input_from_segments, add_special_tokens_
 
 
 class ChatBot:
-    def __init__(self, args, tokenizer, model):
+    '''Conversation Agent model based on Hugging face, using GPT-2'''
+    def __init__(self, args):
+        '''Initialize tokenizer, model and datasets'''
         self.args = args
-        self.tokenizer =tokenizer
-        self.model = model
+        tokenizer_class, model_class = GPT2Tokenizer, GPT2LMHeadModel
+
+        # laod tokenizer and model
+        self.tokenizer = tokenizer_class.from_pretrained(args.model_checkpoint)
+        self.model = model_class.from_pretrained(args.model_checkpoint)
+        self.model.to(args.device)
+        add_special_tokens_(self.model, self.tokenizer)
+
         self.history = []
 
     def return_message(self, sentence : str, personality: list) -> str:
@@ -29,43 +39,7 @@ class ChatBot:
 
 
 
-def top_filtering(logits, top_k=0., top_p=0.9, threshold=-float('Inf'), filter_value=-float('Inf')):
-    """ Filter a distribution of logits using top-k, top-p (nucleus) and/or threshold filtering
-        Args:
-            logits: logits distribution shape (vocabulary size)
-            top_k: <=0: no filtering, >0: keep only top k tokens with highest probability.
-            top_p: <=0.0: no filtering, >0.0: keep only a subset S of candidates, where S is the smallest subset
-                whose total probability mass is greater than or equal to the threshold top_p.
-                In practice, we select the highest probability tokens whose cumulative probability mass exceeds
-                the threshold top_p.
-            threshold: a minimal threshold to keep logits
-    """
-    assert logits.dim() == 1  # Only work for batch size 1 for now - could update but it would obfuscate a bit the code
-    top_k = min(top_k, logits.size(-1))
-    if top_k > 0:
-        # Remove all tokens with a probability less than the last token in the top-k tokens
-      indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
-      logits[indices_to_remove] = filter_value
 
-    if top_p > 0.0:
-        # Compute cumulative probabilities of sorted tokens
-        sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-        cumulative_probabilities = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
-
-        # Remove tokens with cumulative probability above the threshold
-        sorted_indices_to_remove = cumulative_probabilities > top_p
-        # Shift the indices to the right to keep also the first token above the threshold
-        sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
-        sorted_indices_to_remove[..., 0] = 0
-
-        # Back to unsorted indices and set them to -infinity
-        indices_to_remove = sorted_indices[sorted_indices_to_remove]
-        logits[indices_to_remove] = filter_value
-
-        indices_to_remove = logits < threshold
-        logits[indices_to_remove] = filter_value
-
-        return logits
 def sample_sequence(personality, history, tokenizer, model, args, current_output=None):
 
     special_tokens_ids = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS)
