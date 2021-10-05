b59d2eed5746928f7022e924b741cc45026707bb
metterian <dzzy6505@gmail.com>
2021-10-05 06:27:49 +0000

    fix: sample_sequence as package

--- ChatBot.py
+++ ChatBot.py
@@ -4,9 +4,7 @@ from itertools import chain
 import warnings
 import torch
 import torch.nn.functional as F
-from train import add_special_tokens_
-
-from interact import top_filtering
+from interact import top_filtering, sample_sequence
 
 from transformers import OpenAIGPTLMHeadModel, OpenAIGPTTokenizer, GPT2LMHeadModel, GPT2Tokenizer
 from train import SPECIAL_TOKENS, build_input_from_segments, add_special_tokens_
@@ -37,38 +35,3 @@ class ChatBot:
             out_text = self.tokenizer.decode(out_ids, skip_special_tokens=True)
         return out_text
 
-
-
-
-def sample_sequence(personality, history, tokenizer, model, args, current_output=None):
-
-    special_tokens_ids = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS)
-    if current_output is None:
-        current_output = []
-
-    for i in range(args.max_length):
-        instance = build_input_from_segments(personality, history, current_output, tokenizer, with_eos=False)
-
-        input_ids = torch.tensor(instance["input_ids"], device=args.device).unsqueeze(0)
-        token_type_ids = torch.tensor(instance["token_type_ids"], device=args.device).unsqueeze(0)
-
-        logits = model(input_ids, token_type_ids=token_type_ids)
-        if isinstance(logits, tuple):  # for gpt2 and maybe others
-            logits = logits[0]
-        logits = logits[0, -1, :] / args.temperature
-        logits = top_filtering(logits, top_k=args.top_k, top_p=args.top_p)
-        probs = F.softmax(logits, dim=-1)
-
-        prev = torch.topk(probs, 1)[1] if args.no_sample else torch.multinomial(probs, 1)
-        if i < args.min_length and prev.item() in special_tokens_ids:
-            while prev.item() in special_tokens_ids:
-                if probs.max().item() == 1:
-                    warnings.warn("Warning: model generating special token with probability 1.")
-                    break  # avoid infinitely looping over special token
-                prev = torch.multinomial(probs, num_samples=1)
-
-        if prev.item() in special_tokens_ids:
-            break
-        current_output.append(prev.item())
-
-    return current_output
