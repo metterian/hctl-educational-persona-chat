9fe58897742c42ab9b770b9ef3093eaee58ad24b
metterian <dzzy6505@gmail.com>
2021-12-27 18:09:24 +0900

    fix: export dataset file and reload

--- data_processing/build.py
+++ data_processing/build.py
@@ -8,14 +8,24 @@ import re
 import random
 import string
 import spacy
+from tqdm import tqdm, tqdm_pandas
+from setproctitle import setproctitle
+
 #%%
+# environment setting
+tqdm.pandas()
+setproctitle('joon_persona')
+spacy.prefer_gpu(4) # set gpu setting
+nlp = spacy.load("en_core_web_trf", disable = ["tok2vec", "parser", "attribute_ruler", "lemmatizer"])
+
+#%%
+# load dataset
 data_path = os.path.join(os.path.pardir, 'data/translation_eng_kor.xlsx')
 trans = pd.read_excel(data_path, engine='openpyxl')
-nlp = spacy.load("en_core_web_trf")
 #%%
-
+# preprocess the text
 def space_before_eos(sentence: str, tokenizer = nlp):
-    table = str.maketrans({key: " {0} ".format(key) for key in string.punctuation})
+    table = str.maketrans({key: " {0}".format(key) for key in string.punctuation})
     doc = tokenizer(sentence)
     tokens = [token.text.translate(table) if token.pos_ == 'PUNCT' else token.text for token in doc ]
     sentence = " ".join(tokens)
@@ -23,16 +33,17 @@ def space_before_eos(sentence: str, tokenizer = nlp):
     return sentence
 
 #%%
-trans['번역문'] = trans['번역문'].apply(space_before_eos)
+trans['번역문'] = trans['번역문'].progress_apply(space_before_eos)
+# trans['번역문'] = trans['번역문'].apply(space_before_eos)
 trans['번역문'] = trans['번역문'].str.lower()
-
-translations = trans['번역문'].to_list()
-
 #%%
+data_path = os.path.join(os.path.pardir, 'data/translation_eng_kor_eos.xlsx')
+trans = pd.read_excel(data_path, engine='openpyxl')
+translations = trans['번역문'].to_list()
+#%%
+# get top 15 situations
 situ_count = trans.groupby('상황').count().sort_values(by='소분류', ascending=False)
 situ_count['대화수'] = situ_count['번역문'] / 4
-
-
 situations = situ_count.head(15).index.tolist() # 대화수 상위 15개의 상황
 # %%
 situ_persona = {
@@ -103,7 +114,8 @@ situ_persona = {
         "let's order then. I am starving."
 
     ]
-}#%%
+}
+#%%
 def sample_candidate(candidates = translations, num = 18):
     return random.sample(candidates, num)
 # %%
