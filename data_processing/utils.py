4d369abd4ae451c9d8f2060ddb0f0fdae27564a1
metterian <dzzy6505@gmail.com>
2021-12-30 04:44:05 +0000

    fix: get path function for debugging

---
+++ data_processing/utils.py
@@ -0,0 +1,39 @@
+import os
+import spacy
+
+nlp = spacy.load(
+    "en_core_web_sm",  # set the tokenizer
+)
+
+
+def get_absolute_path(path: str, relative=True) -> str:
+    """
+    Refine the file path
+        - relative : for debugging
+    """
+    if relative:
+        return os.path.join(os.path.pardir, path)
+    else:
+        return path
+
+
+def check_punctuation(words):
+    punctuation = [".", ","]
+    return [word for word in words if word in punctuation]
+
+
+# preprocess the text
+def space_before_eos(sentence: str, tokenizer=nlp):
+    """
+    Preprocessing function:
+    - Spaces before periods at end of sentences
+    - everything lowercase
+    """
+    table = str.maketrans({".": " .", ",": " ,"})
+    sentence = sentence.lower().split()
+    for i, word in enumerate(sentence):
+        if check_punctuation(word):
+            for token in tokenizer(word):
+                if token.pos_ == "PUNCT":
+                    sentence[i] = sentence[i].translate(table)
+    return " ".join(sentence)
