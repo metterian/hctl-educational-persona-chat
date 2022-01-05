35dd122023a31244c5b330aad62b3cc0ea569619
metterian <dzzy6505@gmail.com>
2022-01-05 07:48:17 +0000

    fix: model file path

--- config.py
+++ config.py
@@ -15,11 +15,17 @@ class Params:
     device: str
     min_length: int
     seed: int
-    temperature: float
     top_k: int
+    temperature: float
     top_p: float
+
+@dataclass
+class Setting:
+    situation: str
 
 @dataclass
 class ChatConfig:
     files: Files
     params: Params
+    setting: Setting
+
