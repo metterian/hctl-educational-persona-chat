bf5ab5803eab3bd621f20dc663ff110ef295c02e
metterian <dzzy6505@gmail.com>
2022-01-24 16:34:11 +0900

    fix: property for Analysis class, make Loader class

--- data_processing/analysis.py
+++ data_processing/analysis.py
@@ -1,6 +1,8 @@
 #%%
 import json
 from tkinter import dialog
+
+from attrs import setters
 from datasets import DatasetBuilder
 import pandas as pd
 
@@ -35,7 +37,29 @@ from pprint import pprint
 
 
 @dataclass
-class Dataset:
+class Loader:
+    path : str
+
+    def __post_init__(self):
+        self.name = Path(self.path).stem
+        self.path = Path(self.path)
+        with self.path.open() as fp:
+            self._data = json.load(fp)
+
+    @property
+    def train(self):
+        return self._data['train']
+
+    @property
+    def valid(self):
+        return self._data['valid']
+
+    @property
+    def dataset(self):
+        return self._data['train'] + self._data['valid']
+
+@dataclass
+class Analysis:
     '''
     To get data statics of SITUATION CHAT
         - Dialogues
@@ -45,72 +69,85 @@ class Dataset:
     Args:
         - dataset_path: str
     '''
-    path: str
+    dataset: List[dict]
 
     def __post_init__(self):
-        path = Path(self.path)
-        with path.open() as fp:
-            self.data = json.load(fp)
-        self.train = self.data['train']
-        self.valid = self.data['valid']
-        self.name = path.stem
-        self.train_history = self.get_histroy(self.train)
-        self.valid_history = self.get_histroy(self.valid)
-
-    @staticmethod
-    def get_histroy(dataset) -> List[List]:
-        # for dialog in dataset:
-        dialog = dataset[1]
-        # history =  dialog['utterances'][-1]['history']
-        pprint(dialog['utterances'])
-        # return history
+        self.history = self.get_history()
 
 
+    def get_history(self):
+        self._history = []
+        for dialog in self.dataset:
+            gold_history = []
+            last_utt = dialog['utterances'][-1]
+            gold_history = last_utt['history'].copy()
+            gold_history.append(last_utt['candidates'][-1])
+            self._history.append(gold_history)
+        return self._history
 
+    @property
     def num_of_dialogue(self) -> str:
         '''Get number of persona'''
-        return f"Train: {len(self.train)}, Valid: {len(self.valid)}"
+        return f"Dialogue: {len(self.history)}"
 
+    @property
     def num_of_utterance(self) -> str:
         '''Get number of utterance'''
-        train_utt = sum([len(dialogue) for dialogue in self.train_history])
-        valid_utt = sum([len(dialogue) for dialogue in self.valid_history])
-        return f"Train: {train_utt}, Valid: {valid_utt}"
+        num_of_utt = sum([len(dialog) for dialog in self.history])
+        return f"Utterance: {num_of_utt}"
 
+    @property
     def average_turns(self) -> str:
         '''Get average turns'''
-        train_turns = sum([len(dialogue) for dialogue in self.train]) / len(self.train_history)
-        valid_turns = sum([len(dialogue) for dialogue in self.valid]) / len(self.valid_history)
-        return f"Train: {train_turns}, Valid: {valid_turns}"
+        average_turns = sum([len(dialog) for dialog in self.history]) / len(self.history)
+        return f"Average Turns: {average_turns}"
 
-    def count_words(self, dataset) -> int:
-        word_count = 0
-        for dialogue in dataset:
-            word_count += len(dialogue.split())
-        return word_count
+    @property
+    def example(self):
+        return sum([len(dialog['utterances']) for dialog in self.dataset])
 
-    def num_of_word(self) -> str:
-        '''Get number of word'''
-        return f"Train: {self.count_words(self.train)}, Valid: {self.count_words(self.valid)}"
 
-situation_chat = Dataset('../data/situationchat_original.json')
-# persona_chat = Dataset('../data/personachat_self_original.json')
-# print(situation_chat.train_history)
+    def count_words(self) -> int:
+        '''Get number of words in utterance'''
+        words = 0
+        for dialogs in self.history:
+            for dialog in dialogs:
+                words += len(dialog.split())
+        return words
+
+    @property
+    def average_words(self) -> str:
+        '''Get average words'''
+        num_of_utt = sum([len(dialog) for dialog in self.history])
+        average_words = self.count_words() / num_of_utt
+        return f"Average Words: {average_words}"
 
 #%%
-situation_chat = Dataset('../data/situationchat_original.json')
-
-
+situation_chat = Loader('../data/situationchat_original.json')
+persona_chat = Loader('../data/personachat_self_original.json')
+#%%
 datasets = [situation_chat, persona_chat]
-#%%
-
+print("TRAIN: ")
 for dataset in datasets:
+    analysis = Analysis(dataset.train)
     print(f"{dataset.name}")
-    print(f"num_of_dialogue : {dataset.num_of_dialogue()}",
-          f"num_of_utterance : {dataset.num_of_utterance()}",
-          f"average_turns : {dataset.average_turns()}",
-          # TODO: augmented dataset can not work number of word because of number of dialogues
-        #   f"num_of_word: {dataset.num_of_word()}",
+    print(f"num_of_dialogue : {analysis.num_of_dialogue}",
+          f"num_of_utterance : {analysis.num_of_utterance}",
+          f"average_turns : {analysis.average_turns}",
+          f"example : {analysis.example}",
+          f"average_words : {analysis.average_words}",
+          sep='\n')
+    print('-'*20)
+
+print("VALID: ")
+for dataset in datasets:
+    analysis = Analysis(dataset.valid)
+    print(f"{dataset.name}")
+    print(f"num_of_dialogue : {analysis.num_of_dialogue}",
+          f"num_of_utterance : {analysis.num_of_utterance}",
+          f"average_turns : {analysis.average_turns}",
+          f"example : {analysis.example}",
+          f"average_words : {analysis.average_words}",
           sep='\n')
     print('-'*20)
 
